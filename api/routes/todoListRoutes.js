7eb1b13c48107f6aa4a7b7b2603ca9b118bf34ec
Simon Pintado <simon.pintado@mxglobalsolutions.com>
2018-03-12 15:55:44 -0700

    Agrego ruta /task/envio para mandar datos por post

--- api/routes/todoListRoutes.js
+++ api/routes/todoListRoutes.js
@@ -4,7 +4,9 @@ module.exports=function(app){
 
     //Aqui defino las rutas que voy a utilizar en el sistema 
     app.route('/task')
-        .get(todoList.list_all_task)
+        .get(todoList.list_all_task);
+        
+    app.route('/task/enviar')
         .post(todoList.create_a_task);
 
     app.route('/task/:taskId')
